1669d3819d03e1af704d76b001d0a0aa1f9cd90e
João Bolsson <joaovictorbolsson@gmail.com>
2026-05-11 12:08:56 -0300

    Add: word-break-ii - submission-1

---
+++ Data Structures & Algorithms/word-break-ii/submission-1.java
@@ -0,0 +1,69 @@
+/*
+
+["cats","dog","sand","and","cat"]
+
+      * *
+    i
+c a t s a n d o g
+
+- dict needs to have a word that starts with s[0] and a word that ends with s[s.length - 1]
+*/
+
+class Solution {
+
+    class Node {
+        Map<Character, Node> children = new HashMap<>();
+        boolean isWord = false;
+    }
+
+    class Trie {
+        Node root = new Node();
+
+        void add(String w) {
+            var curr = root;
+
+            for (var l : w.toCharArray()) {
+                if (!curr.children.containsKey(l)) curr.children.put(l, new Node());
+                curr = curr.children.get(l);
+            }
+            curr.isWord = true;
+        }
+    }
+
+    // from start -> all sentences
+    Map<Integer, List<String>> memo = new HashMap<>();
+
+    List<String> backtrack(int start, String s, Trie trie, String currW) {
+        if (memo.containsKey(start)) return memo.get(start);
+
+        var curr = trie.root;
+        for (var j = start; j < s.length(); j++) {
+            var l = s.charAt(j);
+            if (!curr.children.containsKey(l)) {
+                break;
+            }
+
+            currW += l;
+
+            curr = curr.children.get(l);
+            if (curr.isWord) {
+                if (j == s.length() - 1) {
+                    memo.computeIfAbsent(start, k -> new ArrayList<String>()).add(currW);
+                } else {
+                    var res = backtrack(j+1, s, trie, "");
+                    if (!res.isEmpty()) {
+                        for (var next : res) if (!next.isBlank()) memo.computeIfAbsent(start, k -> new ArrayList<String>()).add(currW + " " + next);
+                    }
+                }
+            }
+        }
+        return memo.getOrDefault(start, List.of());
+    }
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        var trie = new Trie();
+        for (var w : wordDict) trie.add(w);
+
+        return backtrack(0, s, trie, "");
+    }
+}
